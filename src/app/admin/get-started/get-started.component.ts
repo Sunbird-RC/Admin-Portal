88c3a1e536165f1f9ab221a901a8a3a5cef2499b
Tejash <tejash.jl1020@gmail.com>
2022-08-22 14:43:49 +0530

    added util function to get schemas by reference implementation

--- src/app/admin/get-started/get-started.component.ts
+++ src/app/admin/get-started/get-started.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getAllReferenceSchemas } from '../../../schemas';
 import { Route } from '@angular/router';
 @Component({
   selector: 'get-started',
@@ -10,6 +11,7 @@ export class GetStartedComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(getAllReferenceSchemas());
   }
 
   status: boolean = false;

bd5cfd6c0b9da209a3d4f965de741e104d7e3041
bhanuprakash-egov <bhanu.prakash@egovernments.org>
2022-08-29 17:00:49 +0530

    changing form structure

--- src/app/admin/test-and-verify/test-and-verify.component.ts
+++ src/app/admin/test-and-verify/test-and-verify.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestAndVerifyComponent implements OnInit {
   linkImage: string ="assets/images/thumbnail.png";
-  constructor() { }
+  vaccinationStatus : Array<String>;
+  formdata : any;
+  objectKeys = Object.keys;
+  constructor() { 
+    this.vaccinationStatus = [ "Fully vaccinated", "Partially Vaccinated", "Not Vaccinated" ]
+    this.formdata = {
+      'Issuer Name' : [{'type':'text', 'placeholder':'Issuer Name', 'id':'issuerName'}],
+      'Beneficiary Name' : [{'type':'text', 'placeholder':'Beneficiary Name', 'id':'beneficiaryName'}],
+      'Vaccination Date' : [{'type':'date', 'placeholder':'Vacciantion Date', 'id':'vaccDate'}],
+      'Vaccination Status' : [{'type':'option', 'placeholder':'Vaccination Status', 'id':'vaccStatus'}]
+    }
+  }
 
   ngOnInit(): void {
   }

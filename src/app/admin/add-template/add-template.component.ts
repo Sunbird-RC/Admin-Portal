446a5553d533e64389f171595ab60550d16d65b2
Chaitrali R <chaitrali_r@tekditechnologies.com>
2022-11-18 12:36:37 +0530

    Add Template Changes

--- src/app/admin/add-template/add-template.component.ts
+++ src/app/admin/add-template/add-template.component.ts
@@ -98,35 +98,78 @@ export class AddTemplateComponent implements OnInit {
     localStorage.setItem('content', '');
   }
 
-  previewScreen(doc) {
-    console.log({ doc });
-    this.sampleData = doc;
+  previewScreen() {
+    // console.log({ doc });
+    // this.sampleData = doc;
 
 
-    fetch(doc.schemaUrl)
-      .then(response => response.text())
-      .then(data => {
-        this.schemaContent = data;
-        this.userJson = data;
-        // Do something with your data
-        console.log(this.userJson);
-      });
+    // fetch(doc.schemaUrl)
+    //   .then(response => response.text())
+    //   .then(data => {
+    //     this.schemaContent = data;
+    //     this.userJson = data;
+    //     // Do something with your data
+    //     console.log(this.userJson);
+    //   });
 
-    fetch(doc.certificateUrl)
-      .then(response => response.text())
-      .then(data => {
-        this.certificateContent = data;
-        this.userHtml = data;
-        this.injectHTML();
-      });
+    // fetch(doc.certificateUrl)
+    //   .then(response => response.text())
+    //   .then(data => {
+    //     this.certificateContent = data;
+    //     this.userHtml = data;
+    //     this.injectHTML();
+    //   });
 
     //  this.isPreview = true;
     //  localStorage.setItem('isPreview', 'yes');
+
+    this.userHtml = `<html lang="en">
+    <head>
+      <style>
+        .line {
+          text-align: center;
+          font-family:'Lato';
+          font-size: 2em;
+        }
+      </style>
+    </head>
+    <body style="width: 900px">
+      <div style="margin: 1em;border-radius: 5px;background-color: white;height: 600px;border: 3px solid #B275FF;">
+        <div style="padding:1em;text-align:center;font-family: Lato;font-size: 4em;"> Certificate Title</div>
+        <table width="100%">
+          <tr>
+  
+            <td>
+              <div class="line">
+                <div>Certified to</div>
+                <div>
+                  <b>{{record.name}}</b>
+                </div>
+                <div>issued on</div>
+                <div style="padding: 1em;font-family: 'Open Sans', sans-serif">
+                  {{ credential.issuancedate}}
+                   
+                </div>
+                <div>issued by </div>
+                <div>{{issuer.name}}</div>
+              </div>
+            </td>
+                  <td valign="bottom">
+              <img style="float:left;padding-left: 2em;" src="{{qrCode}}" alt="qr_code" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div></div>
+    </body>
+  </html>
+  `
+  this.injectHTML();
   }
 
   editTemplate() {
-    localStorage.setItem('sampleData', JSON.stringify(this.sampleData));
-   this.router.navigate(['/edit-template/' + this.usecase + '/' +this.entityName], { state: { item: this.sampleData } });
+     localStorage.setItem('sampleData', JSON.stringify(this.userHtml));
+   this.router.navigate(['/edit-template/' + this.usecase + '/' +this.entityName], { state: { item: this.userHtml } });
   }
 
 

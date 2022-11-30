060781e73a6441f095305076461a1cbb7e4d81f8
Namita g <namita_g@tekditechnologies.com>
2022-11-30 15:00:39 +0530

    Fix: config-workflow issue of dropdown data

--- src/app/admin/config-workflow/config-workflow.component.ts
+++ src/app/admin/config-workflow/config-workflow.component.ts
@@ -1,15 +1,192 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { GeneralService } from "src/app/services/general/general.service";
+import { SchemaService } from "src/app/services/data/schema.service";
+import CommonData from "src/assets/schemas/education/Common.json";
 @Component({
-  selector: 'config-workflow',
-  templateUrl: './config-workflow.component.html',
-  styleUrls: ['./config-workflow.component.scss']
+  selector: "config-workflow",
+  templateUrl: "./config-workflow.component.html",
+  styleUrls: ["./config-workflow.component.scss"],
 })
 export class ConfigWorkflowComponent implements OnInit {
+  entityName: any;
+  schemaName = [];
+  schemaName_data = [];
+  entityList = [];
+  fieldList = [];
+  fieldList_data = [];
+  propertyName = [];
+  feildNameList = [];
+  studentFieldList = [];
+  global_properties = [];
+  global_tempName = "";
 
-  constructor() { }
+  global_properties_student = [];
+  global_tempName_student = "";
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    public generalService: GeneralService
+  ) {
+    this.entityName = this.route.snapshot.params.entity;
   }
 
+  ngOnInit(): void {
+    let selectedMenuList: any;
+    this.generalService.getData("/Schema").subscribe((res) => {
+      for (let i = 0; i < res.length; i++) {
+        this.schemaName.push(JSON.parse(res[i]["schema"]));
+        this.entityList.push(this.schemaName[i]["title"]);
+        this.fieldList.push(this.schemaName[i]["definitions"]);
+        selectedMenuList = this.fieldList.find((e) => e[this.entityName]);
+      }
+
+      this.onChangeSelect(this.entityName);
+    });
+  }
+
+  onChangeSelect(item: any) {
+    let arr = [];
+    this.studentFieldList = [];
+    const attest = this.fieldList.find((e) => e[item]);
+    arr = this.getPropertiesStudent(attest?.[item], attest);
+
+    this.studentFieldList.push(arr);
+  }
+
+  getPropertiesStudent(item: any, main_item: any) {
+    this.getPropertiesNameStudent(item, "", Object.keys(main_item), 0);
+    return this.global_properties_student;
+  }
+  getPropertiesNameStudent(
+    item: any,
+    key_name: any,
+    main_item: any,
+    index: any
+  ) {
+    if (!key_name.includes(this.global_tempName_student) && key_name !== "") {
+      key_name = this.global_tempName_student + "." + key_name;
+    }
+    if (item?.properties) {
+      let temp_arr = Object.keys(item?.properties);
+      let temp_mykey = main_item[index]; //Object.getOwnPropertyNames(main_item)[0];
+
+      for (let i = 0; i < temp_arr.length; i++) {
+        if (item?.properties?.[temp_arr[i]]?.properties) {
+          this.getPropertiesNameStudent(
+            item?.properties?.[temp_arr[i]],
+            key_name + temp_mykey + ".",
+            Object.keys(item?.properties),
+            i
+          );
+        } else {
+          this.global_properties_student.push(
+            key_name === temp_mykey
+              ? key_name + "." + temp_arr[i]
+              : key_name + temp_mykey + "." + temp_arr[i]
+          );
+        }
+      }
+    }
+  }
+
+  onSelect(item: any) {
+    let arr = [];
+    this.feildNameList = [];
+    const attest = this.fieldList.find((e) => e[item]);
+    arr = this.getProperties(attest?.[item], attest);
+
+    this.feildNameList.push(arr);
+  }
+
+  getProperties(item: any, main_item: any) {
+    this.global_tempName = Object.keys(main_item)[0];
+    this.getPropertiesName(item, "", Object.keys(main_item), 0);
+    return this.global_properties;
+  }
+  getPropertiesName(item: any, key_name: any, main_item: any, index: any) {
+    if (!key_name.includes(this.global_tempName) && key_name !== "") {
+      key_name = this.global_tempName + "." + key_name;
+    }
+    if (item?.properties) {
+      let temp_arr = Object.keys(item?.properties);
+      let temp_mykey = main_item[index];
+
+      for (let i = 0; i < temp_arr.length; i++) {
+        if (item?.properties?.[temp_arr[i]]?.properties) {
+          this.getPropertiesName(
+            item?.properties?.[temp_arr[i]],
+            key_name + temp_mykey + ".",
+            Object.keys(item?.properties),
+            i
+          );
+        }
+        if (item?.properties?.[temp_arr[i]]?.["items"]?.["$ref"]) {
+          let myArray = "";
+          let feild_name = "";
+          let commonSchema_name =
+            item?.properties?.[temp_arr[i]]?.["items"]?.["$ref"];
+          myArray = commonSchema_name.split("/")[0].split(".")[0];
+          feild_name = commonSchema_name.split("/")[3];
+          const temp_data_array = this.schemaName?.filter(
+            (val) => val.title == myArray
+          );
+          if (temp_data_array) {
+            for (let j = 0; j <= temp_data_array.length; j++) {
+              if (
+                temp_data_array[j]?.["definitions"]?.[feild_name]?.[
+                  "properties"
+                ]
+              ) {
+                this.getPropertiesName(
+                  temp_data_array[j]?.["definitions"]?.[feild_name],
+                  key_name + [temp_arr[i]] + ".",
+                  Object.keys(
+                    temp_data_array[j]?.["definitions"][feild_name][
+                      "properties"
+                    ]
+                  ),
+                  j
+                );
+              }
+            }
+          }
+        }
+        if (item?.properties?.[temp_arr[i]]?.["$ref"]) {
+          let myArray = "";
+          let feild_name = "";
+          let commonSchema_name = item?.properties?.[temp_arr[i]]?.["$ref"];
+          console.log(commonSchema_name);
+          myArray = commonSchema_name.split("/")[0].split(".")[0];
+          feild_name = commonSchema_name.split("/")[3];
+
+          const temp_data_array = this.schemaName?.filter(
+            (val) => val.title == myArray
+          );
+
+          if (temp_data_array) {
+            for (let j = 0; j <= temp_data_array.length; j++) {
+              if (temp_data_array[j]?.["definitions"]) {
+                this.getPropertiesName(
+                  temp_data_array[j]?.["definitions"][feild_name],
+                  key_name + temp_mykey + ".",
+                  Object.keys(
+                    temp_data_array[j]?.["definitions"][feild_name][
+                      "properties"
+                    ]
+                  ),
+                  j
+                );
+              }
+            }
+          }
+        }
+        this.global_properties.push(
+          key_name === temp_mykey
+            ? key_name + "." + temp_arr[i]
+            : key_name + temp_mykey + "." + temp_arr[i]
+        );
+      }
+    }
+  }
 }

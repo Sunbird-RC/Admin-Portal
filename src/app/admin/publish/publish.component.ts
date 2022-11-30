ac04ae38984182e54cd4285bf41104396c37307c
suvarnakale <suvarna.kale@tekditechnologies.com>
2022-11-30 15:09:39 +0530

    publish UI-added table and made it dynamic- by suvarna

--- src/app/admin/publish/publish.component.ts
+++ src/app/admin/publish/publish.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Console, timeStamp } from "console";
 import { GeneralService } from "src/app/services/general/general.service";
 
 @Component({
@@ -9,30 +10,52 @@ import { GeneralService } from "src/app/services/general/general.service";
 export class PublishComponent implements OnInit {
   isShow = false;
   schemaOsid: any;
-  constructor(private generalService: GeneralService) {}
+  publishData: any;
+  constructor(private generalService: GeneralService) { }
 
-  ngOnInit(): void {}
-  ngAfterViewInit() {}
+  ngOnInit(): void {
+    this.generalService.getData("/Schema").subscribe((res) => {
+      for (let i = 0; i < res.length; i++) {
+        this.publishData = res;
+      }
+    });
+
+
+    // this.generalService.getData("/Schema/").subscribe(
+    //   (res)=>{
+    //     for(let j=0; j<res.length; j++){
+    //       this.publishData = res;
+
+
+    //     }
+
+    //   }
+    // )
+
+  }
+  ngAfterViewInit() { }
   async changeVal() {
     this.isShow = !this.isShow;
 
-    this.generalService.getData("/Schema").subscribe((res) => {
-      for (let i = 0; i < res.length; i++) {
-        this.changeStatus(res[i]);
-      }
-    });
+
   }
 
-  async changeStatus(res) {
-    await this.generalService
-      .getData("/Schema/" + res.osid)
+  changeStatus(osid, i) {
+    this.generalService
+      .getData("/Schema/" + osid)
       .subscribe((data) => {
         data["status"] = "PUBLISHED";
         this.generalService
           .putData("/Schema", data.osid, data)
-          .subscribe((data1) => {
-            console.log(data1);
-          });
+          .subscribe((res) => {
+            console.log(res);
+            if (res["params"]["status"] = "SUCCESSFUL") {
+              this.publishData[i]["status"] = 'PUBLISHED';
+            }
+          },
+            (err) => {
+              console.log(err)
+            });
       });
   }
 }

3eabc2b785fd90df4866b0e23010a84acccda149
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2022-12-06 13:04:00 +0530

    Task #1 : Issue fixes

--- src/app/admin/create-vc-template/create-vc-template.component.ts
+++ src/app/admin/create-vc-template/create-vc-template.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { SchemaService } from '../../services/data/schema.service';
 import { GeneralService } from 'src/app/services/general/general.service';
-import { TranslateService } from '@ngx-translate/core'; 
+import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from 'src/app/app.config';
 import { KeycloakService } from 'keycloak-angular';
-
-import { Params } from '@angular/router';
-import { NavigationEnd , NavigationStart } from '@angular/router';
-
 
 @Component({
   selector: 'create-vc-template',
   templateUrl: './create-vc-template.component.html',
   styleUrls: ['./create-vc-template.component.scss']
 })
-export class CreateVcTemplateComponent implements OnInit {
+export class CreateVcTemplateComponent implements OnInit, OnChanges {
+  @Input() usecaseSchema;
   thumbnailItems: any = []
   params: any;
   entityName: any;
@@ -26,9 +23,9 @@ export class CreateVcTemplateComponent implements OnInit {
   isShow1: boolean;
   isShow2: boolean;
   res2: any;
-  items:any = []
+  items: any = []
   userHtml1 = '';
-  credTemp : any = []
+  credTemp: any = []
   baseUrl = this.config.getEnv('baseUrl');
   token: any;
   paramFromRoute: any;
@@ -42,14 +39,23 @@ export class CreateVcTemplateComponent implements OnInit {
     public translate: TranslateService,
     private config: AppConfig,
     public keycloakService: KeycloakService
-  ) { 
-       
+  ) {
+
   }
 
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const latestRequest = changes['usecaseSchema'];
+    if (latestRequest.currentValue) {
+      this.entityName = latestRequest.currentValue;
+      this.loadPage();
+    }
+  }
+
+
   ngOnInit() {
-  
-      this.keycloakService.getToken().then((res)=>{
-        this.token = res;
+    this.keycloakService.getToken().then((res) => {
+      this.token = res;
     });
 
     this.generalService.getData('/Schema').subscribe((res) => {
@@ -57,11 +63,11 @@ export class CreateVcTemplateComponent implements OnInit {
      
       this.getCredTemplate();
       this.injectHTML();
-     });
+    });
 
     this.activeRoute.params.subscribe(params => {
       this.params = params;
-   
+
 
       if (this.params.hasOwnProperty('usecase')) {
         this.usecase = params.usecase;
@@ -90,91 +96,65 @@ export class CreateVcTemplateComponent implements OnInit {
         let temp = window.location.href.split('/');
         this.entityName = temp[temp.length - 1]
       }
-      
-      
-      let schemaVc = localStorage.getItem('schemaVc');
-      if (schemaVc != undefined) {
-        schemaVc = JSON.parse(schemaVc);
-        let self = this;
-        Object.keys(schemaVc).forEach(function (key) {
-          if (key !== 'title') {
 
-            self.schemaName = key;
-            self.vcObject = schemaVc[key];         
-            self.thumbnailItems.push({
-              "thumbnailUrl": "/assets/images/thumbnail.png",
-              "title": self.vcObject.name,
-              "description": self.vcObject.description,
-              "html": self.vcObject.html,
-            })
-          }
-        });
-
-      }
     });
   }
 
-  readSchema(res)
-  {
-    for(let i =0; i < res.length; i++)
-    {
-      if(typeof(res[i].schema)== 'string')
-      {
+  async loadPage() {
+    this.userHtml1 = '';
+    this.credTemp = [];
+    await this.getCredTemplate();
+    this.injectHTML();
+  }
+
+
+  readSchema(res) {
+    for (let i = 0; i < res.length; i++) {
+      if (typeof (res[i].schema) == 'string') {
         res[i].schema = JSON.parse(res[i].schema);
 
-        if( !res[i].schema.hasOwnProperty('isRefSchema') &&  !res[i].schema.isRefSchema){
+        if (!res[i].schema.hasOwnProperty('isRefSchema') && !res[i].schema.isRefSchema) {
           this.items.push(res[i]);
         }
-       
       }
-     
     }
   }
 
-  getCredTemplate(){
-   
-    for(let i =0; i < this.items.length; i++)
-    {
-      if(this.items[i]["name"] == this.entityName)
-      {
+  async getCredTemplate() {
+
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i]["name"] == this.entityName) {
         let a = this.items[i]["schema"]["_osConfig"]["certificateTemplates"];
         let b = Object.values(a);
         let c = b.toString();
         let d = c.split("Schema/");
-        console.log('this.token -> ', this.token);
 
-         this.generalService.getText( '/Schema/' + d[1]).subscribe((res)=>{
-          this.userHtml1 = res; 
+        await this.generalService.getText('/Schema/' + d[1]).subscribe((res) => {
+          this.userHtml1 = res;
           this.credTemp.push({
-         
-            "title":Object.keys(a),    
+            "title": Object.keys(a),
             "html": res
-         
           });
-
-         }, (err)=>{
-           console.log({err});
-         })
-
-        
+        }, (err) => {
+          console.log({ err });
+        })
       }
-
     }
-   
+
   }
 
- 
+
   injectHTML() {
 
     setTimeout(() => {
-      const iframe: HTMLIFrameElement = document.getElementById('iframe1') as HTMLIFrameElement;
+      let iframe: HTMLIFrameElement = document.getElementById('iframe1') as HTMLIFrameElement;
+
 
       var iframedoc;
-      if (iframe.contentDocument)
+      if (iframe['contentDocument'])
         iframedoc = iframe.contentDocument;
-      else if (iframe.contentWindow)
+      else if (iframe['contentWindow'])
         iframedoc = iframe.contentWindow.document;
-  
 
       if (iframedoc) {
         // Put the content in the iframe
@@ -184,9 +164,9 @@ export class CreateVcTemplateComponent implements OnInit {
       } else {
         alert('Cannot inject dynamic contents into iframe.');
       }
-    }, 500)
-  }
+    }, 1000)
 
+  }
 
 
   openAddVc() {

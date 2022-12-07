a32f24351099f8be13ed6b139cff421b12c8246f
suvarnakale <suvarna.kale@tekditechnologies.com>
2022-12-07 17:03:04 +0530

    Swagger json -Show only user created schemas on Explore API-- Issue Fixed

--- src/app/admin/publish/swaggerui/swaggerui.component.ts
+++ src/app/admin/publish/swaggerui/swaggerui.component.ts
@@ -1,19 +1,48 @@
 import { Component, OnInit } from "@angular/core";
 import { SwaggerUIBundle } from "swagger-ui-dist";
+import { GeneralService } from 'src/app/services/general/general.service';
 
 @Component({
   selector: "app-swaggerui",
   templateUrl: "./swaggerui.component.html",
 })
 export class SwaggeruiComponent implements OnInit {
-  constructor() {}
+  token: string;
+
+  constructor(private generalService: GeneralService) { }
 
   ngOnInit(): void {
-    const ui = SwaggerUIBundle({
-      url: "https://demo-admin-portal.xiv.in/registry/api/docs/swagger.json",
-      domNode: document.getElementById("swagger-ui-item"),
-      deepLinking: true,
-      layout: "BaseLayout",
-    });
+    this.openSwaggerUi();
   }
+
+  openSwaggerUi() {
+    this.generalService.getToken().then((res) => {
+      this.token = res;
+      let self = this;
+
+      const ui = SwaggerUIBundle({
+        domNode: document.getElementById("swagger-ui-item"),
+        deepLinking: true,
+        layout: "BaseLayout",
+        url: "https://demo-admin-portal.xiv.in/registry/api/docs/user/swagger.json",
+
+        'responseInterceptor':
+          function (response: any) {
+            if (response.obj.access_token) {
+              console.log(response.obj.access_token);
+              const token = response.obj.access_token;
+              self.token;
+            }
+            return response;
+          },
+        requestInterceptor:
+          function (request) {
+            console.log('[Swagger] intercept try-it-out request');
+            request.headers.Authorization = "Bearer " + self.token;
+            return request;
+          }
+      });
+    })
+  }
+
 }

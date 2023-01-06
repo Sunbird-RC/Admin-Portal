b2ba7e7301bff43eb1d173b6406293f3aa674894
suvarnakale <suvarna.kale@tekditechnologies.com>
2023-01-06 16:23:17 +0530

    veriable names changes

--- src/app/admin/get-started/get-started.component.ts
+++ src/app/admin/get-started/get-started.component.ts
@@ -10,21 +10,21 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class GetStartedComponent implements OnInit {
   attestationCheckbox: boolean =true;
-  VerifiableCredentialCheckbox: boolean = true;
-  OwnershipCheckbox: boolean = false;
+  verifiableCredentialCheckbox: boolean = true;
+  ownershipCheckbox: boolean = false;
   chooseEntity: string;
   attestationCheckboxBorder: string;
-  VCcheckboxBorder: string;
-  OwnershipCheckboxBorder: string;
+  vcCheckboxBorder: string;
+  ownershipCheckboxBorder: string;
 
   constructor(public translate: TranslateService) { }
 
   ngOnInit(): void {
     localStorage.setItem("schemaParams", "");
       
-    if (this.attestationCheckbox == true && this.VerifiableCredentialCheckbox == true) {
+    if (this.attestationCheckbox == true && this.verifiableCredentialCheckbox == true) {
       this.attestationCheckboxBorder = "border-color-sec";
-      this.VCcheckboxBorder = "border-color-sec";
+      this.vcCheckboxBorder = "border-color-sec";
 
       this.checkEntity();
     }
@@ -43,30 +43,30 @@ export class GetStartedComponent implements OnInit {
   }
 
   changeEvent2() {
-    this.VerifiableCredentialCheckbox = !this.VerifiableCredentialCheckbox;
-    if (this.VerifiableCredentialCheckbox) {
-      this.VCcheckboxBorder = "border-color-sec";
+    this.verifiableCredentialCheckbox = !this.verifiableCredentialCheckbox;
+    if (this.verifiableCredentialCheckbox) {
+      this.vcCheckboxBorder = "border-color-sec";
     } else {
-      this.VCcheckboxBorder = "border-color-pri";
+      this.vcCheckboxBorder = "border-color-pri";
     }
     this.checkEntity();
   }
 
   changeEvent3() {
-    this.OwnershipCheckbox = !this.OwnershipCheckbox;
-    if (this.OwnershipCheckbox) {
-      this.OwnershipCheckboxBorder = "border-color-sec";
+    this.ownershipCheckbox = !this.ownershipCheckbox;
+    if (this.ownershipCheckbox) {
+      this.ownershipCheckboxBorder = "border-color-sec";
     } else {
-      this.OwnershipCheckboxBorder = "border-color-pri";
+      this.ownershipCheckboxBorder = "border-color-pri";
     }
     this.checkEntity();
   }
 
    checkEntity(){
     return this.attestationCheckbox ? this.chooseEntity = "attestmodule"
-         : this.VerifiableCredentialCheckbox && !this.OwnershipCheckbox ? this.chooseEntity = "vcmodule"
-         : this.OwnershipCheckbox && !this.VerifiableCredentialCheckbox ? this.chooseEntity = "ownershipmodule" 
-         : this.VerifiableCredentialCheckbox && this.OwnershipCheckbox ? this.chooseEntity = "vcownershipmodule"
+         : this.verifiableCredentialCheckbox && !this.ownershipCheckbox ? this.chooseEntity = "vcmodule"
+         : this.ownershipCheckbox && !this.verifiableCredentialCheckbox ? this.chooseEntity = "ownershipmodule" 
+         : this.verifiableCredentialCheckbox && this.ownershipCheckbox ? this.chooseEntity = "vcownershipmodule"
          : this.chooseEntity = "newmodule" ;
   }
 }

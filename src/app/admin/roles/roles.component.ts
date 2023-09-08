37b7ca07393ba503470d88dc612f9f522520b17c
Prasang023 <prasang023@gmail.com>
2023-09-08 18:40:06 +0530

    added PUT request in attestation workflow save button and formatted code in roles ui to be more readable

--- src/app/admin/roles/roles.component.ts
+++ src/app/admin/roles/roles.component.ts
@@ -1,78 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
-import { GeneralService } from 'src/app/services/general/general.service';
+import { Component, OnInit } from "@angular/core"
+import { ActivatedRoute } from "@angular/router"
+import { TranslateService } from "@ngx-translate/core"
+import { GeneralService } from "src/app/services/general/general.service"
 
 @Component({
-    selector: 'roles-template',
-    templateUrl: './roles.component.html',
-    styleUrls: ['./roles.component.scss']
+	selector: "roles-template",
+	templateUrl: "./roles.component.html",
+	styleUrls: ["./roles.component.scss"]
 })
 export class RolesComponent implements OnInit {
-    params: any;
-    isSettingMenu: boolean = false;
+	params: any
+	isSettingMenu: boolean = false
 
-    status: boolean = false;
-    activeMenu: string = '';
-  sideMenu: any;
-  menus: any;
-  an_menus: any;
-  currentMenu: number = 0;
-  items = [];
-  isLoading : boolean = true;
-  isSchemaCreated: boolean = false;
-constructor(
-  private activeRoute: ActivatedRoute,
-  private generalService: GeneralService,
-  public translate: TranslateService
-) { }
+	status: boolean = false
+	activeMenu: string = ""
+	sideMenu: any
+	menus: any
+	an_menus: any
+	currentMenu: number = 0
+	items = []
+	isLoading: boolean = true
+	isSchemaCreated: boolean = false
+	constructor(
+		private activeRoute: ActivatedRoute,
+		private generalService: GeneralService,
+		public translate: TranslateService
+	) {}
 
-  ngOnInit(): void {
+	ngOnInit(): void {
+		this.activeRoute.params.subscribe((params) => {
+			this.params = params
 
-    this.activeRoute.params.subscribe(params => {
-      this.params = params;
+			if (params.hasOwnProperty("page") && params.page)
+				// this.activeMenu = params.page;
+				console.log({ params })
+		})
 
-      if(params.hasOwnProperty('page') && params.page)
-     // this.activeMenu = params.page;
-      console.log({params});
-    });
+		this.generalService.getData("/Schema").subscribe(
+			(res) => {
+				this.readSchema(res)
+			},
+			(err) => {
+				this.items = []
+				this.isLoading = false
+			}
+		)
+	}
 
-     this.generalService.getData('/Schema').subscribe((res) => {
-     this.readSchema(res);
-    }, (err)=>{
-      this.items = [];
-      this.isLoading = false;
+	readSchema(res) {
+		for (let i = 0; i < res.length; i++) {
+			if (typeof res[i].schema == "string") {
+				res[i].schema = JSON.parse(res[i].schema)
+				this.isSchemaCreated = true
 
-    });
-  }
+				if (
+					!res[i].schema.hasOwnProperty("isRefSchema") &&
+					!res[i].schema.isRefSchema
+				) {
+					this.items.push(res[i])
+				}
 
+				this.isLoading = false
+			}
+		}
+	}
 
-  readSchema(res)
-  {
-    for(let i =0; i < res.length; i++)
-    {
-      if(typeof(res[i].schema)== 'string')
-      {
-        res[i].schema = JSON.parse(res[i].schema);
-        this.isSchemaCreated = true;
-
-        if( !res[i].schema.hasOwnProperty('isRefSchema') &&  !res[i].schema.isRefSchema){
-          this.items.push(res[i]);
-        }
-
-        this.isLoading = false;
-       
-      }
-     
-    }
-  }
-
-  openMenu(menu){
-  }
-
-  clickEvent() {
-    this.status = !this.status;
-  }
-
-
+	clickEvent() {
+		this.status = !this.status
+	}
 }

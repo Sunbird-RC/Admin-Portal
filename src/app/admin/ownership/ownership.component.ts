3b49c39350a9f23f1dba744f25e978faefc7371d
suvarnakale <suvarna.kale@tekditechnologies.com>
2023-10-03 18:56:35 +0530

    #814 - Ownership UI Modifications - Done

--- src/app/admin/ownership/ownership.component.ts
+++ src/app/admin/ownership/ownership.component.ts
@@ -19,32 +19,29 @@ export class OwnershipComponent implements OnInit, OnChanges {
   propertyList = [];
   propertyNames = [];
   selectedProperty: any;
+  entityList: any[];
 
   constructor(
     private activeRoute: ActivatedRoute,
     public translate: TranslateService,
     private fb: FormBuilder,
     public generalService: GeneralService
-  ) { 
+  ) {
     this.ownershipForm = this.fb.group({
-      uniqueField: '',
       primaryLogin: this.fb.group({
-        authenticationType: '',
-        emailCheckbox: false,
-        emailField: false,
-        mobilenumberCheckbox: false,
-        mobilenumberField: ''
-      }),
-      alternateLoginCheckbox: false,
-      alternateLogin: this.fb.group({
-        authenticationType: '',
+        userIDCheckbox : false,
+        userIDField : '',
         emailCheckbox: false,
         emailField: '',
         mobilenumberCheckbox: false,
-        mobilenumberField: ''
+        mobilenumberField: '',
+        passwordCheckbox:false,
+        passwordField: ''
       }),
       issuer: 'anonymous',
-      issuerRole: ''
+      issuerRole: '',
+      invitee:'anonymous',
+      inviteRole:''
     })
   }
 
@@ -52,8 +49,8 @@ export class OwnershipComponent implements OnInit, OnChanges {
     this.activeRoute.params.subscribe(params => {
       this.params = params;
 
-      if(params.hasOwnProperty('page') && params.page)
-      this.isSettingMenu = true;
+      if (params.hasOwnProperty('page') && params.page)
+        this.isSettingMenu = true;
       this.entityName = params.entity;
     });
 
@@ -76,7 +73,6 @@ export class OwnershipComponent implements OnInit, OnChanges {
     for (let i = 0; i < res.length; i++) {
       if (typeof (res[i].schema) == 'string') {
         res[i].schema = JSON.parse(res[i].schema);
-
         if (!res[i].schema.hasOwnProperty('isRefSchema') && !res[i].schema.isRefSchema) {
           this.items.push(res[i]);
         }
@@ -87,72 +83,90 @@ export class OwnershipComponent implements OnInit, OnChanges {
   populateFields() {
     this.propertyList = [];
     this.propertyNames = [];
-    for(let i = 0; i < this.items.length; i++) {
-      // console.log(this.items[i]);
-      if(this.items[i]["name"] == this.entityName) {
+    this.entityList = [];
+    for (let i = 0; i < this.items.length; i++) {
+      this.entityList.push(this.items[i].name);
+      if (this.items[i]["name"] == this.entityName) {
         let properties = this.items[i]["schema"]["definitions"][this.entityName]["properties"];
+        let requiredProperties = this.items[i]["schema"]["definitions"][this.entityName]["required"];
+        for(let j=0; j< requiredProperties.length; j++){
+          this.propertyNames.push(requiredProperties[j]);
+        }
         const keys = Object.keys(properties);
         keys.forEach((key, index) => {
           properties[key] = {
             ...properties[key],
             "key": key
           }
-          this.propertyList.push(properties[key]);
-          this.propertyNames.push(properties[key].title);
+          this.propertyList.push(properties[key]);          
         });
+        
       }
     }
   }
 
   submitOwnershipForm() {
-    if(this.ownershipForm.value.uniqueField.length === 0) {
+    if (this.ownershipForm.value.uniqueField.length === 0) {
       return;
     }
 
-    for(let i = 0; i < this.propertyNames.length; i++) {
-      if(this.ownershipForm.value.uniqueField === this.propertyNames[i]) {
+    for (let i = 0; i < this.propertyNames.length; i++) {
+      if (this.ownershipForm.value.uniqueField === this.propertyNames[i]) {
         this.selectedProperty = this.propertyList[i];
       }
     }
 
-    for(let i = 0; i < this.items.length; i++){
-      if(this.entityName === this.items[i].name) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.entityName === this.items[i].name) {
         let ownershipObj = {};
         let alternateOwnershipObj = {};
         let editSchema = this.items[i]["schema"];
         editSchema["_osConfig"]["ownershipAttributes"] = [];
         editSchema["_osConfig"]["uniqueIndexFields"] = [];
-        
-        if(this.ownershipForm.value.primaryLogin['emailCheckbox']){
+
+        if (this.ownershipForm.value.primaryLogin['userIDCheckbox']) {
+          ownershipObj = {
+            ...ownershipObj,
+            "userID": "/" + this.selectedProperty?.key + "/userID"
+          }
+        }
+        if (this.ownershipForm.value.primaryLogin['emailCheckbox']) {
           ownershipObj = {
             ...ownershipObj,
             "email": "/" + this.selectedProperty?.key + "/email"
           }
         }
-        if(this.ownershipForm.value.primaryLogin['mobilenumberCheckbox']){
+        if (this.ownershipForm.value.primaryLogin['mobilenumberCheckbox']) {
           ownershipObj = {
             ...ownershipObj,
             "mobile": "/" + this.selectedProperty?.key + "/contact"
           }
         }
-        if(Object.keys(ownershipObj).length !== 0){
+        if (this.ownershipForm.value.primaryLogin['passwordCheckbox']) {
+          ownershipObj = {
+            ...ownershipObj,
+            "password": "/" + this.selectedProperty?.key + "/password"
+          }
+        }
+
+        if (Object.keys(ownershipObj).length !== 0) {
           editSchema["_osConfig"]["ownershipAttributes"].push(ownershipObj);
           editSchema["_osConfig"]["uniqueIndexFields"].push(this.selectedProperty.key);
         }
 
-        if(this.ownershipForm.value.alternateLoginCheckbox && this.ownershipForm.value.alternateLogin['emailCheckbox']){
+        if (this.ownershipForm.value.alternateLoginCheckbox && this.ownershipForm.value.alternateLogin['emailCheckbox']) {
           alternateOwnershipObj = {
             ...alternateOwnershipObj,
             "email": "/" + this.selectedProperty?.key + "/email"
           }
         }
-        if(this.ownershipForm.value.alternateLoginCheckbox && this.ownershipForm.value.alternateLogin['mobilenumberCheckbox']){
+        if (this.ownershipForm.value.alternateLoginCheckbox && this.ownershipForm.value.alternateLogin['mobilenumberCheckbox']) {
           alternateOwnershipObj = {
             ...alternateOwnershipObj,
             "mobile": "/" + this.selectedProperty?.key + "/contact"
           }
         }
-        if(Object.keys(alternateOwnershipObj).length !== 0){
+        if (Object.keys(alternateOwnershipObj).length !== 0) {
           editSchema["_osConfig"]["ownershipAttributes"].push(alternateOwnershipObj);
           editSchema["_osConfig"]["uniqueIndexFields"].push(this.selectedProperty.key);
         }
